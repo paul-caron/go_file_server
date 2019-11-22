14f40bed8c6a4767c9e80c210545eeefab40f118
Paul <paul.a.caron@gmail.con>
2019-11-21 22:06:10 -0600

    changed the structure a bit to make life easier

--- server.go
+++ server.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-    "fmt"
+_    "fmt"
     "net/http"
     "log"
     "os"
@@ -26,35 +26,21 @@ func main(){
     port = os.Args[1]
     myServer := &http.Server{
         MaxHeaderBytes : 1 << 20,
-        ReadTimeout : 1 * time.Second,
+        ReadTimeout : 10 * time.Second,
         WriteTimeout : 10 * time.Second,
         Addr : ":" + port,
     }
     http.HandleFunc("/", root)
+    http.HandleFunc("/delete", deleteFile)
+    http.HandleFunc("/upload", uploadFile)
     log.Println("Starting file server in directory : ",dir+":"+port)
     fileServer := http.FileServer(http.Dir("./"))
     http.Handle("/static/", fileServer)
-    log.Fatal(myServer.ListenAndServe())
+    log.Fatal(myServer.ListenAndServeTLS("cert.pem","key.pem"))
 }
 
 
 func root(w http.ResponseWriter, r *http.Request) {
-    if r.Method == "POST" {
-        r.ParseMultipartForm(1<<20)
-        file, handler, err := r.FormFile("upload")
-        if err != nil {
-            log.Println("Error Retrieving the File")
-            log.Println(err)
-        }
-        defer file.Close()
-        b, _ := ioutil.ReadAll(file)
-        ioutil.WriteFile("static/"+handler.Filename, b, 0666)
-    }else if r.Method == "DELETE" {
-         r.ParseForm()
-         os.Remove(r.Form["filepath"][0])
-         fmt.Fprint(w,"deleted")
-         return
-    }
     var files []infoStruct
     filepath.Walk(dir, func (fpath string, finfo os.FileInfo, err error) error {
         if !finfo.IsDir() {
@@ -68,4 +54,27 @@ func root(w http.ResponseWriter, r *http.Request) {
     })
     templ, _ := template.ParseFiles("template.html")
     templ.Execute(w, files)
+}
+
+func deleteFile(w http.ResponseWriter, r *http.Request) {
+    r.ParseForm()
+    if len(r.Form["filepath"]) > 0 {
+        log.Println(os.Remove(r.Form["filepath"][0]))
+        templ, _ := template.ParseFiles("templateDelete.html")
+        templ.Execute(w, "")
+    }
+}
+
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+    r.ParseMultipartForm(1<<20)
+    file, handler, err := r.FormFile("upload")
+    if err != nil {
+        log.Println("Error Retrieving the File")
+        log.Println(err)
+    }
+    defer file.Close()
+    b, _ := ioutil.ReadAll(file)
+    ioutil.WriteFile("static/"+handler.Filename, b, 0666)
+    templ, _ := template.ParseFiles("templateUpload.html")
+    templ.Execute(w, nil)
 }
